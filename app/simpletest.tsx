4f00c220b1bd8b079ae2ef0a1d4fd9a4f7978072
Corey <35475851+cor9@users.noreply.github.com>
2025-06-03 14:17:51 -0700

    Create simpletest.tsx

---
+++ app/simpletest.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from 'react';
+
+export default function SimpleTestPage() {
+  console.log("<<<<<< BUILDING SIMPLETEST PAGE - " + new Date().toISOString() + " >>>>>>");
+  return <h1>Hello from Simple Test Page!</h1>;
+}
