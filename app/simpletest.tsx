bd93010941b82c2899a9ff010ccb6b8cf72b2fb0
Corey <35475851+cor9@users.noreply.github.com>
2025-06-03 19:39:41 -0700

    Delete app/simpletest.tsx

--- app/simpletest.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import React from 'react';
-
-export default function SimpleTestPage() {
-  console.log("<<<<<< BUILDING SIMPLETEST PAGE - " + new Date().toISOString() + " >>>>>>");
-  return <h1>Hello from Simple Test Page!</h1>;
-}
