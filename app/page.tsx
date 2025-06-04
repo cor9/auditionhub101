5ca631b344ce94df47c96e2254be5d7cdeb8be86
Corey <35475851+cor9@users.noreply.github.com>
2025-06-04 06:49:19 -0700

    Fix Learn More Link

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
                   size="lg"
                   className="bg-background"
                 >
-                  <Link href="/about">Learn more</Link>
+                  <Link href="/features">Learn more</Link>
                 </Button>
               </div>
             </div>
