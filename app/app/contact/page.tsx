6b44897c20309815293e23016656270cd7d88514
Corey <35475851+cor9@users.noreply.github.com>
2025-06-12 06:58:50 -0700

    Create page.tsx

---
+++ app/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+      <h1>Contact Us</h1>
+      <p>Contact page coming soon...</p>
+    </div>
+  );
+}
