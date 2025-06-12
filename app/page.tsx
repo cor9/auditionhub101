f73e473f812995871f7fd82d373e62d13a43b2d1
Corey <35475851+cor9@users.noreply.github.com>
2025-06-12 03:29:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,26 @@ import {
 } from "lucide-react";
 import { cn } from "@/lib/utils";
 
+
+import { createClient } from '@/utils/supabase/server'
+import { cookies } from 'next/headers'
+
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
+  return (
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
+}
+
+
 interface FeatureCardProps {
   icon: React.ElementType;
   title: string;
