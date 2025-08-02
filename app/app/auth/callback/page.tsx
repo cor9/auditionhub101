d2fa6ecc9755a0d0ca5a359f2903eca8588f6bc4
Corey <35475851+cor9@users.noreply.github.com>
2025-08-02 03:44:28 -0700

    Create page.tsx

---
+++ app/app/auth/callback/page.tsx
@@ -0,0 +1,16 @@
+import { createServerSupabaseClient } from '@/lib/supabase-server';
+import { redirect } from 'next/navigation';
+
+export default async function AuthCallbackPage() {
+  const supabase = createServerSupabaseClient();
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (session) {
+    redirect('/dashboard');
+  } else {
+    redirect('/login');
+  }
+}
