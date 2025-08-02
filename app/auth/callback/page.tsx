e5de759b3961335a6e48de0b033c5c86171087f3
Corey <35475851+cor9@users.noreply.github.com>
2025-08-02 03:50:50 -0700

    Update page.tsx

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -1,4 +1,4 @@
-import { createServerSupabaseClient } from '@/lib/supabase-server';
+import { createServerSupabaseClient } from '../../../lib/supabase-server';
 import { redirect } from 'next/navigation';
 
 export default async function AuthCallbackPage() {
